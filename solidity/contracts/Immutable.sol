70748c6d023540d77538049f1b64ebdd09a96749
0xgareth <gareth@garethv.xyz>
2022-05-28 15:57:49 +0100

    init contract

---
+++ solidity/contracts/Immutable.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+
+import "../node_modules/hardhat/console.sol";
+
+contract Immutable {
+    constructor() {
+        console.log("wagmi contract");
+    }
+}
