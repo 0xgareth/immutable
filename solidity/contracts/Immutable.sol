badfeacaebcbefb842d56e51247dcd6794877e64
gareth-was-here <gareth@garethveale.dev>
2022-05-30 21:33:14 +0100

    add storage to contract

--- solidity/contracts/Immutable.sol
+++ solidity/contracts/Immutable.sol
@@ -3,7 +3,21 @@
 import "hardhat/console.sol";
 
 contract Immutable {
+    
+    // mapping (address => string) public messages;
+    uint messages;
+
     constructor() {
         console.log("wagmi contract");
     }
+
+    function postMessage() public {
+        console.log('posting a message =]');
+        messages += 1;
+    }
+
+    function getMessages() public view returns (uint) {
+        return messages;
+    }
+
 }
--- solidity/scripts/run.js
+++ solidity/scripts/run.js
@@ -3,6 +3,15 @@ const main = async () => {
     const immutableContract = await immutableContractFactory.deploy();
     await immutableContract.deployed();
     console.log("immutable has been deployed to:", immutableContract.address);
+
+    const waves = await immutableContract.getMessages();
+    console.log('Num of messages:',waves);
+
+    const postTxn = await immutableContract.postMessage();
+    await postTxn.wait()
+
+    const waves2 = await immutableContract.getMessages();
+    console.log('Num of messages:', waves2);
 };
 
 main()
